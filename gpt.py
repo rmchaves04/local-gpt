c6409e4b0c6c9765a6782d475d4233b71f128ac5
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-02-25 17:17:29 -0300

    [feat] opeanai api reqs

---
+++ gpt.py
@@ -0,0 +1,30 @@
+from openai import OpenAI
+from typing import List, Dict
+from dotenv import load_dotenv
+from models import MODELS
+import os
+
+load_dotenv()
+
+class GPT:
+
+    def __init__(self, model: str, messages: List[Dict]):
+        self.model = model
+        self.messages = messages
+        self.client = OpenAI(
+            api_key=os.getenv("OPENAI_API_KEY")
+        )
+
+    def request(self):
+        try:
+            self.validate_model()
+            return self.client.chat.completions.create(
+                model=self.model,
+                messages=self.messages
+            )
+        except Exception as e:
+            print(e)
+
+    def validate_model(self):
+        if self.model not in MODELS:
+            raise ValueError("Invalid model name")
