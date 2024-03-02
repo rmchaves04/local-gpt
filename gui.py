47f4fd2c562627c3e84e7e13c3eaa56752e077c5
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-03-02 13:13:22 -0300

    chat history for session prompts

--- gui.py
+++ gui.py
@@ -17,11 +17,22 @@ def get_default_prompt():
 def clean_response(response):
     return response.choices[0].message.content.strip()
 
+def build_history_prompt(prompt):
+    contents = [msg.get("content") for msg in message_history]
+    return f"This is the history of the last prompts and responses. Use it to get a better understanding of the conversation and answer the next question.{','.join(contents)}\n\n Prompt: {prompt}"
+
 app = Flask(__name__)
+
+message_history = []
 
 @app.route('/')
 def index():
-    return render_template('index.html', available_models = MODELS)
+    return render_template('index.html', available_models = MODELS, message_history=message_history)
+
+@app.route('/clean-history')
+def clean_history():
+    message_history = []
+    return render_template('index.html', available_models = MODELS, message_history=message_history)
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -29,6 +40,7 @@ def submit():
     model = request.form.get('models')
     system_prompt = request.form.get('system_prompt')
     prompt = request.form.get('prompt')
+    history = request.form.get('history')
 
     messages = []
     if system_prompt:
@@ -39,17 +51,26 @@ def submit():
     else:
         messages.append(get_default_prompt())
 
-    messages.append({
-        "role": "user",
-        "content": prompt
-    })
+    if history:
+        messages.append({
+            "role": "system",
+            "content": build_history_prompt(prompt)
+        })
+    else:
+        messages.append({
+            "role": "user",
+            "content": prompt
+        })
+
+    message_history.append({"sender": "You", "content": prompt})
 
     gpt = GPT(model, messages)
     tokenizer = Tokenizer(gpt)
     response = gpt.request()
     cost = tokenizer.calculate_text_cost(response)
     clean_res = clean_response(response)
-    return render_template('submit.html', response=clean_res, cost=cost)
+    message_history.append({"sender": "GPT", "content": clean_res})
+    return render_template('submit.html', response=clean_res, cost=cost, message_history=message_history)
 
 if __name__ == '__main__':
     app.run(debug=True)
