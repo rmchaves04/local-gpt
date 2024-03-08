700f2e15b435af34fc83a2104032d8163ef81153
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-03-08 18:19:11 -0300

    [feat]: base class for models

---
+++ models/gpt.py
@@ -0,0 +1,31 @@
+import os
+from openai import OpenAI
+from typing import List, Dict
+from dotenv import load_dotenv
+from models.model import Model
+
+load_dotenv()
+
+
+MODELS = ["gpt-3.5-turbo-0125", "gpt-4",
+          "gpt-4-turbo-preview", "gpt-4-vision-preview"]
+
+
+class GPT(Model):
+    def __init__(self, model: str, messages: List[Dict]):
+        self.model = model
+        self.messages = messages
+        self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+    def request(self, stream=False):
+        try:
+            self.validate_model()
+            return self.client.chat.completions.create(
+                model=self.model, messages=self.messages, stream=stream
+            )
+        except Exception as e:
+            print(e)
+
+    def validate_model(self):
+        if self.model not in MODELS:
+            raise ValueError("Invalid model name")
