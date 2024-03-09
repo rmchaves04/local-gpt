c66554e09fe225b3be2e2bd4d992159221e2eff7
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-03-09 11:33:38 -0300

    adding more necessary functions to Model base class

--- models/model.py
+++ models/model.py
@@ -2,7 +2,7 @@ from typing import List, Dict
 
 
 class Model:
-    def __init__(self, model: str, messages: List[Dict]):
+    def __init__(self, model: str = "", messages: List[Dict] = []):
         self.model = model
         self.messages = messages
         self.client = self.create_client()
@@ -15,6 +15,15 @@ class Model:
 
     def validate_model(self) -> bool:
         raise ShouldImplement
+
+    def get_model_variations(self) -> List[str]:
+        raise ShouldImplement
+
+    def set_model(self, model: str):
+        self.model = model
+
+    def set_messages(self, messages: List[Dict]):
+        self.messages = messages
 
 
 class ShouldImplement(Exception):
